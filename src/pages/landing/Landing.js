52e0050bcdcc1975133dfb54f15bb0e35dd8359e
Bruce Ambundo <bruceambundo@gmail.com>
2026-03-03 20:57:48 +0300

    WhyUs section hidden

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import './Landing.css'
 import Hero from '../../components/sections/hero/Hero'
-import WhyUs from '../../components/sections/whyus/WhyUs'
 
 export default function Landing() {
   return (
     <section className='landing-page'>
       <Hero />
-      <WhyUs />
     </section>
   )
 }
