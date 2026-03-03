4051a40c94c053aef0f46dde507ec004f405d1e5
Bruce Ambundo <bruceambundo@gmail.com>
2026-03-03 16:56:41 +0300

    Mapping errors fixed

--- src/components/sections/about/About.js
+++ src/components/sections/about/About.js
@@ -36,7 +36,16 @@ export default function About() {
     
   return (
     <section className='about-section'>
-        <h6 className='title' ref={titleSpring}>About The Platform</h6>
+        <animated.h6 
+            className='title' 
+            ref={ref}
+            style={{
+                ...titleSpring,
+                width: '100%'
+            }}
+        >
+            About The Platform
+        </animated.h6>
 
         <div className='about-content'>
             <animated.div
@@ -51,7 +60,6 @@ export default function About() {
                     src={require('../../../videos/robot-unscreen.gif')}
                     alt="About AI"
                 />
-
             </animated.div>
 
             
