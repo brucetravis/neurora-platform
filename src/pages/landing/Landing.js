03c95d6e76c59b02e60ba8a1a2dc010da3a5dd4d
Bruce Ambundo <bruceambundo@gmail.com>
2026-03-03 20:52:20 +0300

    About section hidden for now

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import './Landing.css'
 import Hero from '../../components/sections/hero/Hero'
-import About from '../../components/sections/about/About'
 
 export default function Landing() {
   return (
     <section className='landing-page'>
       <Hero />
-      <About />
     </section>
   )
 }
