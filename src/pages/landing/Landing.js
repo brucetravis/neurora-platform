6fd1a0473888750ada47a6af73b06aa9868d50c7
Bruce Ambundo <bruceambundo@gmail.com>
2026-03-04 07:22:01 +0300

    Why us section re-designed

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Landing.css'
 import Hero from '../../components/sections/hero/Hero'
+import WhyUs from '../../components/sections/whyus/WhyUs'
 
 export default function Landing() {
   return (
     <section className='landing-page'>
       <Hero />
+      <WhyUs />
     </section>
   )
 }
