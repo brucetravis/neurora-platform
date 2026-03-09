f5ca9616df2c50ab10d004027af3208db1f59457
Bruce Ambundo <bruceambundo@gmail.com>
2026-03-09 17:14:14 +0300

    ref placed appropriately in the section

---
+++ src/components/buttons/navBtn/Button.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Button.css'
+import { useNavigate } from 'react-router-dom'
+
+export default function Button({ text }) {
+  
+  // function to navigate to the next page
+  const navigate = useNavigate()
+
+  return (
+    <div>
+      <button
+        onClick={navigate}
+      >
+        { text }
+      </button>
+    </div>
+  )
+}
