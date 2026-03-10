7290f9dece941b7ed2b3ddc34a565284dd35cba1
Bruce Ambundo <bruceambundo@gmail.com>
2026-03-10 07:59:11 +0300

    Navigate function added in the button component and a popr has been created

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Button.css'
+import { useNavigate } from 'react-router-dom'
 
-export default function Button({ text }) {
+export default function Button({ text, link }) {
+
+  // function to navigate to another page
+  const navigate = useNavigate()
+  
   return (
     <div>
-        <button>{ text }</button>
+      <button
+        onClick={() => navigate(link)}
+      >
+        { text }
+      </button>
     </div>
   )
 }
