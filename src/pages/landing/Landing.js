359ab145f9897e22324478dd952490400389387c
Bruce Ambundo <bruceambundo@gmail.com>
2026-03-10 09:07:10 +0300

    Pages scrolling to the top on mount effect added

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Landing.css'
 import Hero from '../../components/sections/hero/Hero'
 import Product from '../../components/sections/product/Product'
@@ -10,6 +10,12 @@ import Success from '../../components/sections/success/Success'
 import News from '../../components/sections/news/News'
 
 export default function Landing() {
+
+  // useEffect to scroll to the top of the page when landing on teh page
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior:"smooth" })
+  }, []) //empty dependency array
+
   return (
     <section className='landing-page'>
       <Hero />
